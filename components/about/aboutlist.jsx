8b0789d808b147ae09fdaa3ce793c616cec501e7
Shiju Nambiar <>
2021-08-04 22:40:19 +0530

    colors and font adjustments

--- components/about/aboutlist.jsx
+++ components/about/aboutlist.jsx
@@ -10,29 +10,20 @@ const AboutMe = () => {
               className={styles.profile}
               src="../../images/profile.jpg"
               alt="Shiju Nambiar"
-              width="180"
-              height="240"
+              width="150"
+              height="200"
             />
           </div>
         </div>
         <div className={styles.text_wrapper}>
           <p className={styles.text_para}>
-            I am a web designer and developer based in India. Web design and
-            development is my passion and my focus is to build performant,
-            responsive, accessible websites with the latest technology there is.
-          </p>
-          <p className={styles.text_para}>
-            I believe it is important to connect with people I am creating
-            things for to understand their perspective and priorities and also
-            it is important to create things for everyone so that no one feels
-            excluded while visiting the websites I create.
-          </p>
-          <p className={styles.text_para}>
-            I have experience working with clients, customers and vendors across
-            various cultural spectrums, leading high performance teams, taking
-            up challenging roles and responsibilities, driving efficiency in
-            technology and operations, building websites and deploying
-            enterprise applications.
+            I am a web designer and developer based in India. I am passionate
+            about building performant, responsive, accessible websites and build
+            unique brand identity. Over the years I have worked with clients,
+            customers and various stakeholders across cultural spectrums,
+            leading high performance teams, taking up challenging roles and
+            responsibilities, driving efficiency in technology and operations,
+            building websites and deploying enterprise applications.
           </p>
         </div>
       </div>
