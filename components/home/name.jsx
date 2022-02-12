a1a99ab0b9c9303bbb04861ac53bf338ddba0e30
Shiju Nambiar <skoodath@gmail.com>
2022-02-12 11:24:54 +0530

    changed value prop

--- components/home/name.jsx
+++ components/home/name.jsx
@@ -5,14 +5,28 @@ const Name = () => {
   return (
     <section className={styles.name}>
       <div className={styles.name_left}>
-        <h1 className={styles.name_text}>Shiju Nambiar</h1>
-      </div>
-      <div className={styles.name_divider}></div>
-      <div className={styles.value}>
-        <p className={styles.value_para}>
-          A Web developer and tech enthusiast based in India, creating modern,
-          accessible and responsive websites.
-        </p>
+        <div className={styles.name_title}>
+          <h3 className={styles.name_text}>Shiju Nambiar</h3>
+          <h1 className={styles.profile_text}>Frontend Web Developer</h1>
+        </div>
+        <div>
+          <div className={styles.profile_mask}></div>
+          <img
+            className={styles.profile}
+            src="../../images/profile.png"
+            alt="Shiju Nambiar"
+            width="100"
+          />
+        </div>
+        <div className={styles.name_right}>
+          <div className={styles.value}>
+            <p className={styles.value_para}>
+              I am web developer and tech enthusiast based out of India. I want
+              to built websites which are accessible, responsive, elegant and
+              purposeful.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
