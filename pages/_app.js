9b8051878c3cb291c5e4016ab8e15e23af633227
Shiju Nambiar <skoodath@gmail.com>
2022-10-26 11:46:48 +0530

    added page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import "../styles/globals.scss";
 import Layout from "../components/layout";
+import { motion } from "framer-motion";
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial={{
+          scale: 0.8,
+          opacity: 0,
+          x: "50%",
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+          x: "0%",
+        }}
+        transition={{
+          duration: 0.5,
+          bounce: 0,
+          damping: 200,
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </Layout>
   );
 }
