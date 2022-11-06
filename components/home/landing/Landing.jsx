c81cc9de40f7560e3b3b58cd694e3ff2ebff3417
Shiju Nambiar <skoodath@gmail.com>
2022-11-06 18:13:28 +0530

    tagline correction

--- components/home/landing/Landing.jsx
+++ components/home/landing/Landing.jsx
@@ -7,9 +7,11 @@ const Landing = () => {
       <div className={styles.landing}>
         <section className={styles.name_wrapper}>
           <h1 className={styles.name_text}>
-            Hi, I 'm <span>shiju</span>, I love{" "}
-            <span style={{ textTransform: "lowercase" }}>creating</span> for the
-            web!
+            Hi! I 'm <span>shiju</span>, I love{" "}
+            <span style={{ textTransform: "lowercase", fontSize: "100%" }}>
+              creating
+            </span>{" "}
+            for the web!
           </h1>
           <ButtonPrimary text="projects" route="projects" />
         </section>
